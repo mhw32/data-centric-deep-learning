0f37e4d65e61be3637b51344f2daec6525a025a1
mhw32 <me@mikewuis.me>
2022-05-15 17:55:07 -0700

    Add flows to week2

---
+++ course/week2/pipeline_project/flows/directionality_flow.py
@@ -0,0 +1,159 @@
+"""
+Flow #3: This flow will train a small (linear) neural network 
+on the MNIST dataset to performance classification, and run an 
+directionality test to measure model robustness.
+"""
+
+import os
+import wandb
+import torch
+import random
+import numpy as np
+from os.path import join
+from pathlib import Path
+from pprint import pprint
+
+from metaflow import FlowSpec, step, Parameter
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+
+from src.system import MNISTDataModule, DigitClassifierSystem
+from src.tests.directionality import MNISTDirectionalityTest
+from src.utils import load_config, to_json
+
+
+class DigitClassifierFlow(FlowSpec):
+  r"""A flow that trains a image classifier to recognize handwritten
+  digit, such as those in the MNIST dataset. Includes a directionality 
+  test on transformed digits to test model robustness.
+
+  Arguments
+  ---------
+  config (str, default: ./config.py): path to a configuration file
+  """
+  config_path = Parameter('config', 
+    help = 'path to config file', default='./configs/directionality_flow.json')
+
+  @step
+  def start(self):
+    r"""Start node.
+    Set random seeds for reproducibility.
+    """
+    random.seed(42)
+    np.random.seed(42)
+    torch.manual_seed(42)
+
+    self.next(self.init_system)
+
+  @step
+  def init_system(self):
+    r"""Instantiates a data module, pytorch lightning module, 
+    and lightning trainer instance.
+    """
+    config = load_config(self.config_path)
+
+    dm = MNISTDataModule(config)
+    system = DigitClassifierSystem(config)
+
+    checkpoint_callback = ModelCheckpoint(
+      dirpath = config.system.save_dir,
+      monitor = 'dev_loss',
+      mode = 'min',    # look for lowest `dev_loss`
+      save_top_k = 1,  # save top 1 checkpoints
+      verbose = True,
+    )
+
+    wandb_logger = WandbLogger(
+      project = config.wandb.project, 
+      offline = False,
+      entity = config.wandb.entity, 
+      name = 'mnist', 
+      save_dir = 'logs/wandb',
+      config = config)
+
+    trainer = Trainer(
+      max_epochs = config.system.optimizer.max_epochs,
+      logger = wandb_logger,
+      callbacks = [checkpoint_callback])
+
+    self.dm = dm
+    self.system = system
+    self.trainer = trainer
+
+    self.next(self.train_model)
+
+  @step
+  def train_model(self):
+    """Calls `fit` on the trainer."""
+
+    self.trainer.fit(self.system, self.dm)
+
+    wandb.finish()  # close wandb run
+
+    self.next(self.offline_test)
+
+  @step
+  def offline_test(self):
+    r"""Calls (offline) `test` on the trainer. Saves results to a log file."""
+
+    # Load the best checkpoint and compute results using `self.trainer.test`
+    self.trainer.test(self.system, self.dm, ckpt_path = 'best')
+    results = self.system.test_results
+
+    # print results to command line
+    pprint(results)
+
+    log_file = join(Path(__file__).resolve().parent.parent, 
+      f'logs/directionality_flow', 'offline-test-results.json')
+
+    os.makedirs(os.path.dirname(log_file), exist_ok = True)
+    to_json(results, log_file)  # save to disk
+
+    self.next(self.directionality_test)
+
+  @step
+  def directionality_test(self):
+    r"""Runs an directionality test. Saves results to a log file."""
+
+    test = MNISTDirectionalityTest()
+    test.test(self.trainer, self.system)
+
+    results = self.system.test_results
+    pprint(results)
+
+    log_file = join(Path(__file__).resolve().parent.parent, 
+      f'logs/directionality_flow', 'directionality-test-results.json')
+
+    os.makedirs(os.path.dirname(log_file), exist_ok = True)
+    to_json(results, log_file)
+
+    self.results = results
+    self.next(self.end)
+
+  @step
+  def end(self):
+    """End node!"""
+    print('done! great work!')
+
+
+if __name__ == "__main__":
+  """
+  To validate this flow, run `python directionality_flow.py`. To list
+  this flow, run `python directionality_flow.py show`. To execute
+  this flow, run `python directionality_flow.py run`.
+
+  You may get PyLint errors from `numpy.random`. If so,
+  try adding the flag:
+
+    `python directionality_flow.py --no-pylint run`
+
+  If you face a bug and the flow fails, you can continue
+  the flow at the point of failure:
+
+    `python directionality_flow.py resume`
+  
+  You can specify a run id as well.
+  """
+  flow = DigitClassifierFlow()
---
+++ course/week2/pipeline_project/flows/hparam_flow.py
@@ -0,0 +1,152 @@
+"""
+Flow #2: This flow will train a multilayer perceptron with a 
+width found by hyperparameter search.
+"""
+
+import os
+import wandb
+import torch
+import random
+import shutil
+import numpy as np
+from os.path import join
+from pathlib import Path
+from pprint import pprint
+
+from metaflow import FlowSpec, step, Parameter
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+
+from src.system import MNISTDataModule, DigitClassifierSystem
+from src.utils import load_config, to_json
+
+
+class DigitClassifierFlow(FlowSpec):
+  r"""A flow that trains a image classifier to recognize handwritten
+  digit, such as those in the MNIST dataset. We search over three 
+  potential different widths.
+
+  Arguments
+  ---------
+  config (str, default: ./config.py): path to a configuration file
+  """ 
+  config_path = Parameter('config', 
+    help = 'path to config file', default='./configs/hparam_flow.json')
+
+  @step
+  def start(self):
+    r"""Start node.
+    Set random seeds for reproducibility.
+    """
+    random.seed(42)
+    np.random.seed(42)
+    torch.manual_seed(42)
+
+    self.widths = [16, 32, 64]
+
+    # for each width, we will train a model
+    self.next(self.init_and_train, foreach='widths')
+
+  @step
+  def init_and_train(self):
+    r"""Instantiates a data module, pytorch lightning module, 
+    and lightning trainer instance. Calls `fit` on the trainer.
+
+    This should remind of you of `init_system` and `train_model`
+    from Flow #1. We merge the two into one node.
+    """
+    config = load_config(self.config_path)
+    config.system.model.width = self.input
+
+    dm = MNISTDataModule(config)
+    system = DigitClassifierSystem(config)
+
+    checkpoint_callback = ModelCheckpoint(
+      dirpath = os.path.join(config.system.save_dir, f'width{self.input}'),
+      monitor = 'dev_loss',
+      mode = 'min',
+      save_top_k = 1,
+      verbose = True,
+    )
+
+    trainer = Trainer(
+      max_epochs = config.system.optimizer.max_epochs,
+      callbacks = [checkpoint_callback])
+
+    trainer.fit(system, dm)
+
+    self.dm = dm
+    self.system = system
+    self.trainer = trainer
+    self.callback = checkpoint_callback
+
+    self.next(self.find_best)
+
+  @step
+  def find_best(self, inputs):
+    r"""Only keep the system with the lowest `dev_loss."""
+
+    # manually propagate class variables through but we only
+    # need a few of them so no need to call `merge_artifacts`
+    self.dm = inputs[0].dm
+    scores = [input.callback.best_model_score for input in inputs]
+    
+    # find the index of the highest score
+    best_index = np.argmax(scores)
+    
+    # get the best system / trainer
+    # we drop the callback
+    self.system = inputs[best_index].system
+    self.trainer = inputs[best_index].trainer
+
+    # save the best width
+    self.best_width = inputs[best_index].widths[best_index]
+
+    # only the best model proceeds to offline test
+    self.next(self.offline_test)
+
+  @step
+  def offline_test(self):
+    r"""Calls (offline) `test` on the trainer. Saves results to a log file."""
+
+    self.trainer.test(self.system, self.dm, ckpt_path = 'best')
+    results = self.system.test_results
+    results['best_width'] = self.best_width
+
+    pprint(results)
+
+    log_file = join(Path(__file__).resolve().parent.parent, 
+      f'logs/hparam_flow', 'offline-test-results.json')
+
+    os.makedirs(os.path.dirname(log_file), exist_ok = True)
+    to_json(results, log_file)  # save to disk
+
+    self.next(self.end)  
+
+  @step
+  def end(self):
+    """End node!"""
+    print('done! great work!')
+
+
+if __name__ == "__main__":
+  """
+  To validate this flow, run `python hparam_flow.py`. To list
+  this flow, run `python hparam_flow.py show`. To execute
+  this flow, run `python hparam_flow.py run`.
+
+  You may get PyLint errors from `numpy.random`. If so,
+  try adding the flag:
+
+    `python hparam_flow.py --no-pylint run`
+
+  If you face a bug and the flow fails, you can continue
+  the flow at the point of failure:
+
+    `python hparam_flow.py resume`
+  
+  You can specify a run id as well.
+  """
+  flow = DigitClassifierFlow()
