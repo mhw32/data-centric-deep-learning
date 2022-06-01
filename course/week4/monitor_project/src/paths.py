f230608c743fc78e7e7c1f24f7ae7f661b30d9db
mhw32 <me@mikewuis.me>
2022-06-01 11:03:59 -0700

    up

--- course/week4/monitor_project/src/paths.py
+++ course/week4/monitor_project/src/paths.py
@@ -4,7 +4,7 @@ from os.path import realpath, dirname, join
 SRC_DIR = realpath(dirname(__file__))
 ROOT_DIR = realpath(join(SRC_DIR, '..'))
 ARTIFACT_DIR = realpath(join(ROOT_DIR, 'artifacts'))
-DATA_DIR = realpath(join(ARTIFACT_DIR, 'data'))
+DATA_DIR = realpath(join(ROOT_DIR, 'data'))
 LOG_DIR = realpath(join(ARTIFACT_DIR, 'log'))
 SCRIPT_DIR = realpath(join(ROOT_DIR, 'scripts'))
 CONFIG_DIR = realpath(join(ROOT_DIR, 'configs'))
