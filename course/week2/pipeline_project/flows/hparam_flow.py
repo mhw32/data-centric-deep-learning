b0d1d41b5baac089a768b82a9b5b22ee904cb671
Hyeungshik Jung <hyungshike@gmail.com>
2023-04-01 05:17:57 +0000

    week2

--- course/week2/pipeline_project/flows/hparam_flow.py
+++ course/week2/pipeline_project/flows/hparam_flow.py
@@ -22,156 +22,160 @@ from src.utils import load_config, to_json
 
 
 class DigitClassifierFlow(FlowSpec):
-  r"""A flow that trains a image classifier to recognize handwritten
-  digit, such as those in the MNIST dataset. We search over three 
-  potential different widths.
+    r"""A flow that trains a image classifier to recognize handwritten
+    digit, such as those in the MNIST dataset. We search over three 
+    potential different widths.
 
-  Arguments
-  ---------
-  config (str, default: ./config.py): path to a configuration file
-  """ 
-  config_path = Parameter('config', 
-    help = 'path to config file', default='./configs/hparam_flow.json')
-
-  @step
-  def start(self):
-    r"""Start node.
-    Set random seeds for reproducibility.
+    Arguments
+    ---------
+    config (str, default: ./config.py): path to a configuration file
     """
-    random.seed(42)
-    np.random.seed(42)
-    torch.manual_seed(42)
+    config_path = Parameter('config',
+                            help='path to config file', default='./configs/hparam_flow.json')
 
-    self.widths = [16, 32, 64]
+    @step
+    def start(self):
+        r"""Start node.
+        Set random seeds for reproducibility.
+        """
+        random.seed(42)
+        np.random.seed(42)
+        torch.manual_seed(42)
 
-    # for each width, we will train a model
-    self.next(self.init_and_train, foreach='widths')
+        self.widths = [16, 32, 64]
 
-  @step
-  def init_and_train(self):
-    r"""Instantiates a data module, pytorch lightning module, 
-    and lightning trainer instance. Calls `fit` on the trainer.
+        # for each width, we will train a model
+        self.next(self.init_and_train, foreach='widths')
 
-    This should remind of you of `init_system` and `train_model`
-    from Flow #1. We merge the two into one node.
-    """
-    config = load_config(self.config_path)
-    config.system.model.width = self.input
+    @step
+    def init_and_train(self):
+        r"""Instantiates a data module, pytorch lightning module, 
+        and lightning trainer instance. Calls `fit` on the trainer.
 
-    dm = MNISTDataModule(config)
-    system = DigitClassifierSystem(config)
+        This should remind of you of `init_system` and `train_model`
+        from Flow #1. We merge the two into one node.
+        """
+        config = load_config(self.config_path)
+        config.system.model.width = self.input
 
-    checkpoint_callback = ModelCheckpoint(
-      dirpath = os.path.join(config.system.save_dir, f'width{self.input}'),
-      monitor = 'dev_loss',
-      mode = 'min',
-      save_top_k = 1,
-      verbose = True,
-    )
+        dm = MNISTDataModule(config)
+        system = DigitClassifierSystem(config)
 
-    trainer = Trainer(
-      max_epochs = config.system.optimizer.max_epochs,
-      callbacks = [checkpoint_callback])
+        checkpoint_callback = ModelCheckpoint(
+            dirpath=os.path.join(config.system.save_dir, f'width{self.input}'),
+            monitor='dev_loss',
+            mode='min',
+            save_top_k=1,
+            verbose=True,
+        )
 
-    trainer.fit(system, dm)
+        trainer = Trainer(
+            max_epochs=config.system.optimizer.max_epochs,
+            callbacks=[checkpoint_callback])
 
-    self.dm = dm
-    self.system = system
-    self.trainer = trainer
-    self.callback = checkpoint_callback
+        trainer.fit(system, dm)
 
-    self.next(self.find_best)
+        self.dm = dm
+        self.system = system
+        self.trainer = trainer
+        self.callback = checkpoint_callback
 
-  @step
-  def find_best(self, inputs):
-    r"""Only keep the system with the lowest `dev_loss."""
+        self.next(self.find_best)
 
-    # manually propagate class variables through but we only
-    # need a few of them so no need to call `merge_artifacts`
-    self.dm = inputs[0].dm
+    @step
+    def find_best(self, inputs):
+        r"""Only keep the system with the lowest `dev_loss."""
 
-    scores = []        # populate with scores from each hparams
-    best_index = None  # replace with best index
-    
-    # ================================
-    # FILL ME OUT
-    # 
-    # Aggregate the best validation performance across inputs into
-    # the variable `scores`.
-    # 
-    # HINT: the `callback` object has a property `best_model_score`
-    #       that make come in handy. 
-    # 
-    # Then, compute the index of the model and store it in `best_index`.
-    # 
-    # Pseudocode:
-    # --
-    # aggregate scores using `inputs`
-    # best_index = ...
-    #
-    # Type:
-    # --
-    # scores: List[float] 
-    # best_index: integer 
-    # ================================
+        # manually propagate class variables through but we only
+        # need a few of them so no need to call `merge_artifacts`
+        self.dm = inputs[0].dm
 
-    # sanity check for scores length
-    assert len(scores) == len(list(inputs)), "Hmm. Incorrect length for scores."
-    # sanity check for best_index
-    assert best_index is not None
-    assert best_index >= 0 and best_index < len(list(inputs))
-    
-    # get the best system / trainer
-    # we drop the callback
-    self.system = inputs[best_index].system
-    self.trainer = inputs[best_index].trainer
+        scores = []        # populate with scores from each hparams
+        best_index = None  # replace with best index
 
-    # save the best width
-    self.best_width = inputs[best_index].widths[best_index]
+        # ================================
+        # FILL ME OUT
+        #
+        # Aggregate the best validation performance across inputs into
+        # the variable `scores`.
+        #
+        # HINT: the `callback` object has a property `best_model_score`
+        #       that make come in handy.
+        #
+        # Then, compute the index of the model and store it in `best_index`.
+        #
+        # Pseudocode:
+        # --
+        # aggregate scores using `inputs`
+        # best_index = ...
+        #
+        # Type:
+        # --
+        # scores: List[float]
+        # best_index: integer
+        # ================================
 
-    # only the best model proceeds to offline test
-    self.next(self.offline_test)
+        scores = [inp.callback.best_model_score for inp in inputs]
+        best_index = scores.index(max(scores))
 
-  @step
-  def offline_test(self):
-    r"""Calls (offline) `test` on the trainer. Saves results to a log file."""
+        # sanity check for scores length
+        assert len(scores) == len(
+            list(inputs)), "Hmm. Incorrect length for scores."
+        # sanity check for best_index
+        assert best_index is not None
+        assert best_index >= 0 and best_index < len(list(inputs))
 
-    self.trainer.test(self.system, self.dm, ckpt_path = 'best')
-    results = self.system.test_results
-    results['best_width'] = self.best_width
+        # get the best system / trainer
+        # we drop the callback
+        self.system = inputs[best_index].system
+        self.trainer = inputs[best_index].trainer
 
-    pprint(results)
+        # save the best width
+        self.best_width = inputs[best_index].widths[best_index]
 
-    log_file = join(Path(__file__).resolve().parent.parent, 
-      f'logs/hparam_flow', 'offline-test-results.json')
+        # only the best model proceeds to offline test
+        self.next(self.offline_test)
 
-    os.makedirs(os.path.dirname(log_file), exist_ok = True)
-    to_json(results, log_file)  # save to disk
+    @step
+    def offline_test(self):
+        r"""Calls (offline) `test` on the trainer. Saves results to a log file."""
 
-    self.next(self.end)  
+        self.trainer.test(self.system, self.dm, ckpt_path='best')
+        results = self.system.test_results
+        results['best_width'] = self.best_width
 
-  @step
-  def end(self):
-    """End node!"""
-    print('done! great work!')
+        pprint(results)
+
+        log_file = join(Path(__file__).resolve().parent.parent,
+                        f'logs/hparam_flow', 'offline-test-results.json')
+
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+        to_json(results, log_file)  # save to disk
+
+        self.next(self.end)
+
+    @step
+    def end(self):
+        """End node!"""
+        print('done! great work!')
 
 
 if __name__ == "__main__":
-  """
-  To validate this flow, run `python hparam_flow.py`. To list
-  this flow, run `python hparam_flow.py show`. To execute
-  this flow, run `python hparam_flow.py run`.
+    """
+    To validate this flow, run `python hparam_flow.py`. To list
+    this flow, run `python hparam_flow.py show`. To execute
+    this flow, run `python hparam_flow.py run`.
 
-  You may get PyLint errors from `numpy.random`. If so,
-  try adding the flag:
+    You may get PyLint errors from `numpy.random`. If so,
+    try adding the flag:
 
-    `python hparam_flow.py --no-pylint run`
+      `python hparam_flow.py --no-pylint run`
 
-  If you face a bug and the flow fails, you can continue
-  the flow at the point of failure:
+    If you face a bug and the flow fails, you can continue
+    the flow at the point of failure:
 
-    `python hparam_flow.py resume`
-  
-  You can specify a run id as well.
-  """
-  flow = DigitClassifierFlow()
+      `python hparam_flow.py resume`
+
+    You can specify a run id as well.
+    """
+    flow = DigitClassifierFlow()
--- course/week2/pipeline_project/src/tests/regression.py
+++ course/week2/pipeline_project/src/tests/regression.py
@@ -25,154 +25,159 @@ from src.system import DigitClassifierSystem, MNISTDataModule
 
 @torch.no_grad()
 def build_regression_test(system, loader):
-  """Given a trained PyTorch Lightning system, this function will 
-  identify the elements in the dev set where the system 'got it the 
-  most wrong'. We measure the 'incorrectness' using the loss function. 
-  The examples with the highest loss represent those where the model's
-  beliefs least match true label.
+    """Given a trained PyTorch Lightning system, this function will 
+    identify the elements in the dev set where the system 'got it the 
+    most wrong'. We measure the 'incorrectness' using the loss function. 
+    The examples with the highest loss represent those where the model's
+    beliefs least match true label.
 
-  Your task is to return the top 100 dev examples with highest loss. 
+    Your task is to return the top 100 dev examples with highest loss. 
 
-  Arguments
-  ---------
-  system (pytorch_lightning.Module): a PyTorch Lightning system. 
-    See `system.py` for the `DigitClassifierSystem` implementation.
-  loader (torch.utils.data.DataLoader): loader for return minibatches
-    of MNIST data. See `MNISTDataModule`.
+    Arguments
+    ---------
+    system (pytorch_lightning.Module): a PyTorch Lightning system. 
+      See `system.py` for the `DigitClassifierSystem` implementation.
+    loader (torch.utils.data.DataLoader): loader for return minibatches
+      of MNIST data. See `MNISTDataModule`.
 
-  Returns
-  -------
-  images (torch.FloatTensor): stacked image tensors 
-    shape: 100 x 1 x 28 x 28
-  labels (torch.LongTensor): tensor of labels
-    shape: 100
-  """
-  losses = []
-  is_correct = []
+    Returns
+    -------
+    images (torch.FloatTensor): stacked image tensors 
+      shape: 100 x 1 x 28 x 28
+    labels (torch.LongTensor): tensor of labels
+      shape: 100
+    """
+    losses = []
+    is_correct = []
 
-  pbar = tqdm(total = len(loader), leave = True, position = 0)
-  for batch in loader:
-    labels = batch[1]  # these are the true labels!
-    # these are unnormalized probabilities the model predicts
-    logits = system.predict_step(batch[0])
-    # the actual prediction is the argmax of the logits
-    preds = torch.argmax(logits, dim=1)
+    pbar = tqdm(total=len(loader), leave=True, position=0)
+    for batch in loader:
+        labels = batch[1]  # these are the true labels!
+        # these are unnormalized probabilities the model predicts
+        logits = system.predict_step(batch[0])
+        # the actual prediction is the argmax of the logits
+        preds = torch.argmax(logits, dim=1)
 
-    batch_is_correct = []
-    batch_loss = []
-    # ================================
-    # FILL ME OUT
-    # 
-    # Your task is to add elements to `batch_is_correct` and `batch_loss`. 
-    # 
-    # For the first one, we want a list of booleans, one for each example. 
-    # Each value represents whether the model made a correct prediction (1 
-    # if the model was right and 0 if the model was wrong). It will likely 
-    # look something like:
-    #
-    #   batch_is_correct = [1, 1, 0, 1, 0, 0, ...]
-    # 
-    # For the second one, compute the loss between the predicted logit and 
-    # the true label, and store this in `losses`. For instance:
-    # 
-    #   batch_loss = [0.012, 0.718, 0.241, ...]
-    # 
-    # HINT: By default, `F.cross_entropy` will take the sum over the minibatch, 
-    # which you may not want it to do. To prevent this sum, try:
-    # 
-    #   `F.cross_entropy(logits, labels, reduction='none')`
-    # 
-    # As another hint, make sure that `batch_is_correct` and `batch_loss` are 
-    # python lists (not numpy arrays and not torch tensors). To convert a 
-    # torch tensor to a list, use: `x.numpy().tolist()`. 
-    # 
-    # Our solution is 2-3 lines of code. 
-    # 
-    # Pseudocode:
-    # -- 
-    # batch_loss = ...
-    # convert batch_loss to list of floats
-    # 
-    # Type:
-    # --
-    # batch_loss: List[float] (not a torch.Tensor!)
-    #   List of losses for each minibatch element
-    # batch_is_correct: List[int] (not a torch.Tensor!)
-    #   List of integers - 1 if the model got that element correct 
-    #                    - 0 if the model got that element incorrect
-    # ================================
-    losses.extend(batch_loss)
-    is_correct.extend(batch_is_correct)
+        batch_is_correct = []
+        batch_loss = []
+        # ================================
+        # FILL ME OUT
+        #
+        # Your task is to add elements to `batch_is_correct` and `batch_loss`.
+        #
+        # For the first one, we want a list of booleans, one for each example.
+        # Each value represents whether the model made a correct prediction (1
+        # if the model was right and 0 if the model was wrong). It will likely
+        # look something like:
+        #
+        #   batch_is_correct = [1, 1, 0, 1, 0, 0, ...]
+        #
+        # For the second one, compute the loss between the predicted logit and
+        # the true label, and store this in `losses`. For instance:
+        #
+        #   batch_loss = [0.012, 0.718, 0.241, ...]
+        #
+        # HINT: By default, `F.cross_entropy` will take the sum over the minibatch,
+        # which you may not want it to do. To prevent this sum, try:
+        #
+        #   `F.cross_entropy(logits, labels, reduction='none')`
+        #
+        # As another hint, make sure that `batch_is_correct` and `batch_loss` are
+        # python lists (not numpy arrays and not torch tensors). To convert a
+        # torch tensor to a list, use: `x.numpy().tolist()`.
+        #
+        # Our solution is 2-3 lines of code.
+        #
+        # Pseudocode:
+        # --
+        # batch_loss = ...
+        # convert batch_loss to list of floats
+        #
+        # Type:
+        # --
+        # batch_loss: List[float] (not a torch.Tensor!)
+        #   List of losses for each minibatch element
+        # batch_is_correct: List[int] (not a torch.Tensor!)
+        #   List of integers - 1 if the model got that element correct
+        #                    - 0 if the model got that element incorrect
+        # ================================
+        batch_loss = F.cross_entropy(logits, labels, reduction='none').tolist()
+        batch_is_correct = (preds == labels).int().tolist()
 
-    pbar.update()
-  pbar.close()
+        losses.extend(batch_loss)
+        is_correct.extend(batch_is_correct)
 
-  losses = np.array(losses)
-  is_correct = np.array(is_correct)
+        pbar.update()
+    pbar.close()
 
-  losses_incorrect = losses[is_correct == 0]
-  indices = np.argsort(losses_incorrect)[::-1][:100]
+    losses = np.array(losses)
+    is_correct = np.array(is_correct)
 
-  images = []
-  labels = []
-  # Use `loader.dataset.__getitem__` to fetch the images and labels for 
-  # each index in `indices`.
-  for index in indices:
-    image, label = loader.dataset.__getitem__(index)
-    images.append(image)
-    labels.append(label)
-  images = torch.stack(images)
-  labels = torch.LongTensor(labels)
+    losses_incorrect = losses[is_correct == 0]
+    indices = np.argsort(losses_incorrect)[::-1][:100]
 
-  return images, labels
+    images = []
+    labels = []
+    # Use `loader.dataset.__getitem__` to fetch the images and labels for
+    # each index in `indices`.
+    for index in indices:
+        image, label = loader.dataset.__getitem__(index)
+        images.append(image)
+        labels.append(label)
+    images = torch.stack(images)
+    labels = torch.LongTensor(labels)
+
+    return images, labels
 
 
 class MNISTRegressionTest(BaseTest):
-  """A regression test includes a set of 100 known examples that 
-  previous models struggled with. For this project, we include  
-  examples that a linear regression model on MNIST misclassified. 
-  """
-  def __init__(self):
-    super().__init__()
+    """A regression test includes a set of 100 known examples that 
+    previous models struggled with. For this project, we include  
+    examples that a linear regression model on MNIST misclassified. 
+    """
 
-    test_dir = join(self.root, 'regression')
-    data = torch.load(join(test_dir, 'test-data.pt'))
-    images = data['images']
-    labels = data['labels']
+    def __init__(self):
+        super().__init__()
 
-    dataset =  TensorDataset(images, labels)
-    assert len(dataset) == 100, "Unexpected dataset size."
-    self.dataset = dataset
+        test_dir = join(self.root, 'regression')
+        data = torch.load(join(test_dir, 'test-data.pt'))
+        images = data['images']
+        labels = data['labels']
 
-  def get_dataloader(self, batch_size = 10):
-    loader = DataLoader(self.dataset, batch_size=batch_size)
-    return loader
+        dataset = TensorDataset(images, labels)
+        assert len(dataset) == 100, "Unexpected dataset size."
+        self.dataset = dataset
 
-  def test(self, trainer, system):
-    loader = self.get_dataloader()
-    # Pass the dataloader to the trainer and call `test`.
-    # Our solution is one line of code
-    trainer.test(system, dataloaders = loader)
+    def get_dataloader(self, batch_size=10):
+        loader = DataLoader(self.dataset, batch_size=batch_size)
+        return loader
+
+    def test(self, trainer, system):
+        loader = self.get_dataloader()
+        # Pass the dataloader to the trainer and call `test`.
+        # Our solution is one line of code
+        trainer.test(system, dataloaders=loader)
 
 
 if __name__ == "__main__":
-  # Given the checkpoint file for a trained system, build a regression
-  # file and save it to disk.
-  import os
-  import argparse
-  parser = argparse.ArgumentParser()
-  parser.add_argument('ckpt', type=str, help='path to trained checkpoint file')
-  args = parser.parse_args()
+    # Given the checkpoint file for a trained system, build a regression
+    # file and save it to disk.
+    import os
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'ckpt', type=str, help='path to trained checkpoint file')
+    args = parser.parse_args()
 
-  system = DigitClassifierSystem.load_from_checkpoint(args.ckpt)
-  dm = MNISTDataModule(system.config)
-  loader = dm.val_dataloader()
+    system = DigitClassifierSystem.load_from_checkpoint(args.ckpt)
+    dm = MNISTDataModule(system.config)
+    loader = dm.val_dataloader()
 
-  images, labels = build_regression_test(system, loader)
+    images, labels = build_regression_test(system, loader)
 
-  cur_dir = os.path.dirname(__file__)
-  save_path = join(cur_dir, 'images/regression/test-data.pt')
-  os.makedirs(os.path.dirname(save_path), exist_ok=True)
-  torch.save({'images': images, 'labels': labels}, save_path)
+    cur_dir = os.path.dirname(__file__)
+    save_path = join(cur_dir, 'images/regression/test-data.pt')
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    torch.save({'images': images, 'labels': labels}, save_path)
 
-  print(f'Saved to {save_path}.')
+    print(f'Saved to {save_path}.')
