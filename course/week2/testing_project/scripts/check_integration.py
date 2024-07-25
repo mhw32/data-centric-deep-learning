fbcb385201c69aba634b8dae52556c94e9de0b46
cjd2119 <cjd2119@columbia.edu>
2024-07-25 10:12:55 +0000

    completes integration test

--- course/week2/testing_project/scripts/check_integration.py
+++ course/week2/testing_project/scripts/check_integration.py
@@ -4,16 +4,17 @@ from testing.paths import IMAGE_DIR
 
 
 def main():
-  '''Check if examples are found for the integration test.
-  '''
-  test_dir = join(IMAGE_DIR, "integration")
-  try:
-    dataset = MNISTIntegrationTest(test_dir)
-    num_examples = len(dataset.paths)
-    print(f'# of examples: {num_examples}')
-  except:
-    print('Something went wrong? Double check your files.')
+    '''Check if examples are found for the integration test.
+    '''
+    test_dir = join(IMAGE_DIR, "integration")
+    print(test_dir)
+    try:
+        dataset = MNISTIntegrationTest(test_dir)
+        num_examples = len(dataset.paths)
+        print(f'# of examples: {num_examples}')
+    except:
+        print('Something went wrong? Double check your files.')
 
 
 if __name__ == "__main__":
-  main()
+    main()
--- course/week2/testing_project/testing/integration.py
+++ course/week2/testing_project/testing/integration.py
@@ -64,8 +64,8 @@ class MNISTIntegrationTest(BaseTest):
     # Notes:
     # --
     # Nothing to return here
-    pass  # remove me
-    # ================================
+    loader = self.get_dataloader()
+    trainer.test(system, dataloaders=loader)
 
 
 class MNISTIntegrationDataset(Dataset):
